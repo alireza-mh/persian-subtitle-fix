9944a2c08923d19f42b3b337a9630077a9c7193b
Alireza <alireza.mh@gmail.com>
2019-09-08 20:33:15 +0430

    feat: Add Interactive mode

--- main.ts
+++ main.ts
@@ -1,19 +1,46 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const iconv = require('iconv-lite');
+const inquirer = require('inquirer');
+const PathPrompt = require('inquirer-path').PathPrompt;
 
 let inputIndex: number = process.argv.indexOf('-I') || process.argv.indexOf('--input');
 let outputIndex: number = process.argv.indexOf('-O') || process.argv.indexOf('--output');
 
-let inputPathFile: string = (inputIndex > 0 && process.argv[inputIndex + 1] ) ? process.argv[inputIndex + 1] : null;
-let outputFile: string = (inputIndex > 0 && process.argv[outputIndex + 1] ) ? process.argv[outputIndex + 1] : `${inputPathFile} fixed`;
+if (inputIndex>= 0 && outputIndex>= 0) {
+    const inputPathFile = (inputIndex > 0 && process.argv[inputIndex + 1]) ? process.argv[inputIndex + 1] : null;
+    let outputPathFile: string = (inputIndex > 0 && process.argv[outputIndex + 1]) ? process.argv[outputIndex + 1] : `${inputPathFile} fixed`;
 
-outputFile = !outputFile.includes('.srt') ? `${outputFile}.srt` : outputFile;
+    outputPathFile = !outputPathFile.includes('.srt') ? `${outputPathFile}.srt` : outputPathFile;
 
-if (!inputPathFile) throw Error('🚨  You should specify input value with either -I or --input flag');
+    if (!inputPathFile) throw Error('🚨  You should specify input value with either -I or --input flag');
 
-const buffer: Buffer = fs.readFileSync(inputPathFile);
+    ReadDecodeSave(inputPathFile, outputPathFile);
+} else {
+    const questions = [
+        {
+            type: 'path',
+            name: 'inputPathFile',
+            message: 'Please enter subtitle relative file path.\n',
+        },
+        {
+            type: 'path',
+            name: 'outputPathFile',
+            message: 'Please enter output relative file path',
+        },
+    ];
+    inquirer.registerPrompt('path', PathPrompt);
+    inquirer.prompt(questions).then(answers => {
+        let {inputPathFile, outputPathFile} = answers;
+        ReadDecodeSave(inputPathFile, outputPathFile);
+    });
+}
 
-const decodedSub: string = iconv.decode(buffer, 'cp1256');
+function ReadDecodeSave(inputPathFile: string, outputFile: string): void {
 
-fs.writeFileSync(outputFile, decodedSub, 'UTF-8');
+    const buffer: Buffer = fs.readFileSync(inputPathFile);
+
+    const decodedSub: string = iconv.decode(buffer, 'cp1256');
+
+    fs.writeFileSync(outputFile, decodedSub, 'UTF-8');
+}

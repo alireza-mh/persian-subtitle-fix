6698ea7f9f18bb1d0cff9c61165c9c15bc4f4935
Alireza <alireza.mh@gmail.com>
2019-09-09 22:56:55 +0430

    chore: Add Doc - Refactor

--- main.ts
+++ main.ts
@@ -4,9 +4,11 @@ const iconv = require('iconv-lite');
 const inquirer = require('inquirer');
 const PathPrompt = require('inquirer-path').PathPrompt;
 
-let inputIndex: number = process.argv.indexOf('-I') || process.argv.indexOf('--input');
-let outputIndex: number = process.argv.indexOf('-O') || process.argv.indexOf('--output');
+// TODO: add commander
+const inputIndex: number = process.argv.indexOf('-I') || process.argv.indexOf('--input');
+const outputIndex: number = process.argv.indexOf('-O') || process.argv.indexOf('--output');
 
+/* check if any argument specified */
 if (inputIndex>= 0 && outputIndex>= 0) {
     const inputPathFile = (inputIndex > 0 && process.argv[inputIndex + 1]) ? process.argv[inputIndex + 1] : null;
     let outputPathFile: string = (inputIndex > 0 && process.argv[outputIndex + 1]) ? process.argv[outputIndex + 1] : `${inputPathFile} fixed`;
@@ -16,7 +18,9 @@ if (inputIndex>= 0 && outputIndex>= 0) {
     if (!inputPathFile) throw Error('🚨  You should specify input value with either -I or --input flag');
 
     ReadDecodeSave(inputPathFile, outputPathFile);
-} else {
+}
+/* in case no argument specified run interactive mode */
+else {
     const questions = [
         {
             type: 'path',
@@ -26,21 +30,26 @@ if (inputIndex>= 0 && outputIndex>= 0) {
         {
             type: 'path',
             name: 'outputPathFile',
-            message: 'Please enter output relative file path',
+            message: 'Please enter output relative file path.\n',
+            default: process.cwd()
         },
     ];
     inquirer.registerPrompt('path', PathPrompt);
     inquirer.prompt(questions).then(answers => {
         let {inputPathFile, outputPathFile} = answers;
+        outputPathFile = !outputPathFile.includes('.srt') ? `${outputPathFile}.srt` : outputPathFile;
         ReadDecodeSave(inputPathFile, outputPathFile);
     });
 }
 
-function ReadDecodeSave(inputPathFile: string, outputFile: string): void {
-
+/**
+ * ReadDecodeSave
+ * @description Read input file decode it and save it on outputFilePath
+ * @param inputPathFile
+ * @param outputPathFile
+ */
+function ReadDecodeSave(inputPathFile: string, outputPathFile: string): void {
     const buffer: Buffer = fs.readFileSync(inputPathFile);
-
     const decodedSub: string = iconv.decode(buffer, 'cp1256');
-
-    fs.writeFileSync(outputFile, decodedSub, 'UTF-8');
+    fs.writeFileSync(outputPathFile, decodedSub, 'UTF-8');
 }
